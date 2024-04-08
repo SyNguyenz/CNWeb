bb4b9261f66668d3177e8b7bb61d0eb75c010a85
nghiangong <78186214+nghiangong@users.noreply.github.com>
2024-04-08 22:33:16 +0700

    tạo khung trang Admin

---
+++ front-end/src/pages/AdminPage/AdminPage.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { UserOutlined, ProductOutlined } from "@ant-design/icons";
+import { Menu } from "antd";
+import AdminUser from "../../componet/AdminUser/AdminUser";
+import AdminProduct from "../../componet/AdminProduct/AdminProduct";
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+const items = [
+  getItem("Sản phẩm", "products", <ProductOutlined />, ),
+  getItem("Người dùng", "users", <UserOutlined />, ),
+];
+const getLevelKeys = (items1) => {
+  const key = {};
+  const func = (items2, level = 1) => {
+    items2.forEach((item) => {
+      if (item.key) {
+        key[item.key] = level;
+      }
+      if (item.children) {
+        return func(item.children, level + 1);
+      }
+    });
+  };
+  func(items1);
+  return key;
+};
+const levelKeys = getLevelKeys(items);
+const App = () => {
+  const [keySelected, setKeySelected] = useState('users');
+
+  const handleOnClick = ({ item, key, keyPath, domEvent }) => {
+    setKeySelected(key);
+    console.log('key=', key);
+  };
+  const renderPage = (key) => {
+    switch (key) {
+      case 'users':
+        return (
+          <AdminUser />
+        )
+      case 'products':
+        return (
+          <AdminProduct />
+        )  
+      default:
+        return <></>  
+    }    
+  }
+
+  return (
+    <div style={{ display: "flex" }}>
+      <Menu
+        mode="inline"
+        defaultSelectedKeys={[keySelected]}
+        style={{
+          width: 256,
+          height: "100vh",
+        }}
+        items={items}
+        onClick={handleOnClick}
+      />
+      <div>
+        {renderPage(keySelected)}
+      </div>
+    </div>
+  );
+};
+export default App;
--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -1,11 +1,13 @@
 import Home from '../pages/Home';
 import Order from '../pages/Order';
+import AdminPage from '../pages/AdminPage/AdminPage';
 
 // public Routes
 
 const publicRoutes=[
     {path: '/', component: Home},
     {path: '/Order', component: Order},
+    {path: '/AdminPage', component: AdminPage},
 ];
 
 const privateRoutes=[
