28f1340d350b2dbf5f65af53754725f0316bfc5e
namvu0823 <vuvannamb1@gmail.com>
2024-05-15 01:42:46 +0700

    fix Order page

--- front-end/src/pages/Order/index.js
+++ front-end/src/pages/Order/index.js
@@ -1,12 +1,17 @@
 import React, { useContext } from 'react';
 import CartContext from '../../components/CartContext/CartContext';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import './Order.css';
 
 const Order = () => {
     const { cart, addToCart, removeFromCart } = useContext(CartContext);
 
     const formatPrice = (price) => {
+        if( price===null  || price===undefined){
+            return 'N/A';
+        }
         return price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
     };
 
@@ -29,18 +34,26 @@ const Order = () => {
         <div className="cart-page">
             <h1>Giỏ hàng của bạn</h1>
             {cart.length === 0 ? (
-              <Link to="/">
-                <button className="btn-go-home">Tiếp tục mua sắm</button>
-            </Link>
+                <div className='Empty-cart'>
+                    <div className="icon-container">
+                        <FontAwesomeIcon icon={faCartShopping} flip="horizontal" size="6x" color="red" />
+                    </div>
+                    <p>Giỏ hàng của bạn trống</p>
+                     <Link to="/">
+                         <button className="btn-go-home">Tiếp tục mua sắm</button>
+                    </Link>
+               </div>
             ) : (
                 <div className="cart-items">
-                    {cart.map((item) => (
+                    {cart.map((item) => {
+                        const selectedVariant = item;
+                        return (
                         <div key={item.id} className="cart-item">
                             <img src={item.images[0]} alt={item.name} />
                             <div className="cart-item-details">
                                 <h2>{item.name}</h2>
-                                <p>Màu sắc: {item.selectedVariant.color}</p>
-                                <p>Giá: {formatPrice(item.selectedVariant.price)}</p>
+                                <p>Màu sắc: {selectedVariant ? selectedVariant.color : 'N/A'}</p>
+                                <p>Giá: {selectedVariant && selectedVariant.price !== undefined ? formatPrice(selectedVariant.price) : 'N/A'}</p>
                                 <div className="quantity-controls">
                                     <button onClick={() => handleQuantityChange(item.id, -1)}>-</button>
                                     <p>Số lượng: {item.quantity}</p>
@@ -49,7 +62,7 @@ const Order = () => {
                                 <button onClick={() => handleRemoveFromCart(item.id)}>Xóa</button>
                             </div>
                         </div>
-                    ))}
+                    )})}
                 </div>
             )}
         </div>
