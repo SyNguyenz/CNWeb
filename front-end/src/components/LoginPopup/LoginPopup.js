ab0127c83b2033e041e4e833fb44522e22cd2e97
yinnxing <yinnxing@gmail.com>
2024-05-26 21:54:09 +0700

    fix PopupLogin

--- front-end/src/components/LoginPopup/LoginPopup.js
+++ front-end/src/components/LoginPopup/LoginPopup.js
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './LoginPopup.css'; 
 import computer from '../Assets/computer.png'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const LoginPopup = ({ onLogin, onClose }) => {
   return (
     <div className="login-popup-container">
       <div className="login-popup">
         <div className="popup-content">
+          <button className = "button-close"onClick = {onClose}>
+            <FontAwesomeIcon icon={faTimes} />
+          </button> 
+          
           <p>Vui lòng đăng nhập để thực hiện chức năng này!</p>
           <div className='popup-box'>
             <img src={computer}/>
